d43baea6ae84918ec990007804ed6893815c477a
Benjamin Blankholm <snurresvup@hotmail.com>
2014-02-04 12:49:23 +0100

    Initial commit

---
+++ src/TextRemoveEvent.java
@@ -0,0 +1,12 @@
+
+public class TextRemoveEvent extends MyTextEvent {
+
+	private int length;
+	
+	public TextRemoveEvent(int offset, int length) {
+		super(offset);
+		this.length = length;
+	}
+	
+	public int getLength() { return length; }
+}
