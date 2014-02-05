05a2a08465dd293010ff53c99d4dc9e8a2a2d32d
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-02-05 23:11:20 +0100

    Auto indented

--- src/DocumentEventCapturer.java
+++ src/DocumentEventCapturer.java
@@ -4,12 +4,12 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 
 /**
- * 
+ *
  * This class captures and remembers the text events of the given document on
  * which it is put as a filter. Normally a filter is used to put restrictions
  * on what can be written in a buffer. In out case we just use it to see all
  * the events and make a copy. 
- * 
+ *
  * @author Jesper Buus Nielsen
  *
  */
@@ -26,42 +26,42 @@ public class DocumentEventCapturer extends DocumentFilter {
      */
     protected LinkedBlockingQueue<MyTextEvent> eventHistory = new LinkedBlockingQueue<MyTextEvent>();
 
-    /**	
+    /**
      * If the queue is empty, then the call will block until an element arrives.
      * If the thread gets interrupted while waiting, we throw InterruptedException.
-     * 
+     *
      * @return Head of the recorded event queue. 
      */
     MyTextEvent take() throws InterruptedException {
-	return eventHistory.take();
+        return eventHistory.take();
     }
-    
+
     public void insertString(FilterBypass fb, int offset,
-			     String str, AttributeSet a)
-	throws BadLocationException {
+                             String str, AttributeSet a)
+            throws BadLocationException {
 	
 	/* Queue a copy of the event and then modify the textarea */
-	eventHistory.add(new TextInsertEvent(offset, str));		
-	super.insertString(fb, offset, str, a);
-    }	
-    
-    public void remove(FilterBypass fb, int offset, int length) 					
-	throws BadLocationException {
-	/* Queue a copy of the event and then modify the textarea */
-	eventHistory.add(new TextRemoveEvent(offset, length));		
-	super.remove(fb, offset, length);
+        eventHistory.add(new TextInsertEvent(offset, str));
+        super.insertString(fb, offset, str, a);
     }
-    
+
+    public void remove(FilterBypass fb, int offset, int length)
+            throws BadLocationException {
+	/* Queue a copy of the event and then modify the textarea */
+        eventHistory.add(new TextRemoveEvent(offset, length));
+        super.remove(fb, offset, length);
+    }
+
     public void replace(FilterBypass fb, int offset,
-			int length, 
-			String str, AttributeSet a)
-	throws BadLocationException {
+                        int length,
+                        String str, AttributeSet a)
+            throws BadLocationException {
 	
 	/* Queue a copy of the event and then modify the text */
-	if (length > 0) {
-	    eventHistory.add(new TextRemoveEvent(offset, length));		
-	}		
-	eventHistory.add(new TextInsertEvent(offset, str));				
-	super.replace(fb, offset, length, str, a);
-    }    
+        if (length > 0) {
+            eventHistory.add(new TextRemoveEvent(offset, length));
+        }
+        eventHistory.add(new TextInsertEvent(offset, str));
+        super.replace(fb, offset, length, str, a);
+    }
 }
