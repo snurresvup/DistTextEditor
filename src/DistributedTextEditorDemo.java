3db37fa6b7e502e3ee3f81e1654ddf8a72b4ed10
snurresvup <snurresvup@hotmail.com>
2014-02-09 22:38:07 +0100

    Adds demo class

---
+++ src/DistributedTextEditorDemo.java
@@ -0,0 +1,6 @@
+public class DistributedTextEditorDemo {
+    public static void main(String[] args) {
+        DistributedTextEditor dte1 = new DistributedTextEditor();
+        DistributedTextEditor dte2 = new DistributedTextEditor();
+    }
+}
