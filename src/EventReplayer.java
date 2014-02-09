63120f24baf2f95e8285213053f0c111dae0a999
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-02-09 22:20:07 +0100

    Moar comments

--- src/EventReplayer.java
+++ src/EventReplayer.java
@@ -32,12 +32,15 @@ public class EventReplayer implements Runnable {
     public void run() {
         while (true) {
             try {
+                // Our changed implementation switches between two states, depending of wheter we are connected or not.
                 MyTextEvent mte;
                 if (!replayingRemoteEvents) {
                     mte = dec.take();
                 } else {
                     mte = receivedEvents.take();
                 }
+
+                // Begin: This code is not changed
                 if (mte instanceof TextInsertEvent) {
                     final TextInsertEvent tie = (TextInsertEvent)mte;
                     EventQueue.invokeLater(new Runnable() {
@@ -67,14 +70,22 @@ public class EventReplayer implements Runnable {
                         }
                     });
                 }
+                // End: This code is not changed
+
             } catch (InterruptedException ie) {
                 useRemoteOrLocalEvents();
             }
         }
     }
 
+    /**
+     * This method sets the state according to the connected status of the text editor
+     */
     private void useRemoteOrLocalEvents() {
         if (dte.isConnected()) {
+            // If the text editor is connected we start receiving and sending events. Events in the queue of the
+            // document event capturer, are sent through an object stream. Events are received and put in the
+            // receivedEvents queue, and afterwards replayed as usual.
             replayingRemoteEvents = true;
             sender = new Thread(new EventSender(dte.getSocket(), dec, this));
             sender.start();
@@ -82,21 +93,29 @@ public class EventReplayer implements Runnable {
             receiver.start();
             run();
         } else {
+            // Here we simply switch back to the normal mode.
             replayingRemoteEvents = false;
             dte.getArea2().setText("");
             run();
         }
     }
 
+    /**
+     * This method is called when an exception is raised from the object input stream because of a closed.
+      */
     public void handleClosedSocket() {
+        // First we ensure that the sender thread is terminated.
         sender.interrupt();
         if (dte.isConnected()) {
+            // We change the connected state of the text editor, then we update the event replayer to reflect the change
             dte.setConnected(false);
             dte.interruptEventReplayer();
         }
         if (dte.getServer() != null) {
+            // In the case that we are acting as server we return to the listening state.
             dte.startConnectionListener();
         } else {
+            // If we are a client we reset the text area and set the title to disconnected.
             dte.resetText();
             dte.setTitle("Disconnected");
         }
--- src/MyTextEvent.java
+++ src/MyTextEvent.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
  * @author Jesper Buus Nielsen
  *
  */
+// We made this class implement Serializable, so that we could send its object through an object stream
 public class MyTextEvent implements Serializable {
 	MyTextEvent(int offset) {
 		this.offset = offset;
