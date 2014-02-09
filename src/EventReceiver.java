1d47d547335532c180c9e8881fccf096a1b6d57c
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-02-09 22:29:15 +0100

    Last comments

--- src/EventReceiver.java
+++ src/EventReceiver.java
@@ -18,8 +18,10 @@ public class EventReceiver implements Runnable {
     public void run() {
         try {
             if (socket != null) {
+                // We initialise an object input stream from the socket
                 ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
                 while (true) {
+                    // Then we start listening for incoming MyTextEvents. Upon receiving one we add it to the queue.
                     MyTextEvent mte = (MyTextEvent) in.readObject();
                     receivedEvents.add(mte);
                 }
@@ -27,6 +29,8 @@ public class EventReceiver implements Runnable {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            // If the socket is closed from one of the sides we catch this IOException on both sides, and then we
+            // handle things so that the client goes back to local mode, and the server starts listening again.
             er.handleClosedSocket();
         }
     }
--- src/EventSender.java
+++ src/EventSender.java
@@ -18,17 +18,19 @@ public class EventSender implements Runnable {
     public void run() {
         try {
             if (socket != null) {
+                // We initialize an object output stream from the socket.
                 out = new ObjectOutputStream(socket.getOutputStream());
                 while (true) {
+                    // we then start sending the MyTextEvents from the queue in the DocumentEventCapturer
                     out.writeObject(dec.take());
                     out.flush();
                 }
             }
         } catch (Exception e) {
+            // If we catch an IOException or InterruptedException we close the stream and terminate.
             try {
                 out.close();
             } catch (IOException e1) {
-                // e1.printStackTrace();
             }
         }
     }
