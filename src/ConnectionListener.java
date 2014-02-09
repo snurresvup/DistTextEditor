b7786c8b7bcffb789c434a7219dfd4cc4d9397b9
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-02-09 21:42:56 +0100

    Comments for connection listener

--- src/ConnectionListener.java
+++ src/ConnectionListener.java
@@ -12,18 +12,22 @@ public class ConnectionListener implements Runnable {
     @Override
     public void run() {
         try {
+            // Reset upper textfield and make it uneditable
             dte.resetText();
             dte.getArea1().setEditable(false);
+            // Set the title and start listening
             dte.setTitle("I'm listening on " + dte.getLocalHostAddress() +
                     ":" + dte.getPortNumber());
             dte.setSocket(dte.getServer().accept());
+            // Upon connection we set the connected flag and change the title. Then we interupt the event replayer so
+            // that it notices the new state, and changes its behaviour. The text field is now editable again.
             dte.setConnected(true);
             dte.setTitle("Connected to " +
                     dte.getSocket().getRemoteSocketAddress().toString());
             dte.interruptEventReplayer();
             dte.getArea1().setEditable(true);
         } catch (IOException ioe) {
-            System.out.println("Server stopped listening");
+            // Do nothing
         }
     }
 }
