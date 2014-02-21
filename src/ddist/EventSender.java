c8823c487873d3cc9e43b4267e76758242f2340a
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-02-21 10:14:10 +0100

    Makes the event sender send general events

--- src/ddist/EventSender.java
+++ src/ddist/EventSender.java
@@ -5,6 +5,7 @@ import ddist.events.text.MyTextEvent;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static java.lang.Thread.interrupted;
 
@@ -13,9 +14,28 @@ public class EventSender implements Runnable{
     private DocumentEventCapturer dec;
     private Socket socket;
     private ObjectOutputStream outputStream;
+    private LinkedBlockingQueue queue;
 
     public EventSender(DocumentEventCapturer dec) {
         this.dec = dec;
+        socket = null;
+        queue = new LinkedBlockingQueue();
+        receiveLocalEvents();
+    }
+
+    private void receiveLocalEvents() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!interrupted()) {
+                    try {
+                        queue.put(dec.take());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     public void newConnection(Socket socket) {
@@ -31,17 +51,17 @@ public class EventSender implements Runnable{
     public void run() {
         while (!interrupted()) {
             try {
-                MyTextEvent mte = dec.take();
-                sendEvent(mte);
+                Object event = queue.take();
+                sendEvent(event);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void sendEvent(MyTextEvent mte) {
+    private void sendEvent(Object event) {
         try {
-            outputStream.writeObject(mte);
+            outputStream.writeObject(event);
         } catch (IOException e) {
             e.printStackTrace();
         }
