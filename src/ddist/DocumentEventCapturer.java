7e1c37f5e6caf0bcfe3d15e9d692af01ea23ab76
Benjamin Blankholm <snurresvup@hotmail.com>
2014-03-06 13:31:05 +0100

    Commented big bitch algo

--- src/ddist/DocumentEventCapturer.java
+++ src/ddist/DocumentEventCapturer.java
@@ -65,6 +65,12 @@ public class DocumentEventCapturer extends DocumentFilter {
         }
     }
 
+    /*
+     * Updates the offset of TextRemoveEvents and TextInsertEvents, and the length of TextRemoveEvents.
+     * According to un-executed local events.
+     * @param event
+     * The event to be updated.
+     */
     private void updateOffset(TextEvent event) {
         if(event instanceof TextInsertEvent) {
             updateOffsetOfInsertEvent((TextInsertEvent)event);
@@ -74,31 +80,75 @@ public class DocumentEventCapturer extends DocumentFilter {
     }
 
     private void updateOffsetOfRemoveEvent(TextRemoveEvent event) {
+        /*
+         * For every unexecuted local remove event the new event's offset and length has to be updated
+         * according to the following cases.
+         * e = unexecuted event.
+         */
         for(TextEvent e: unExecutedTextEvents) {
+            /*
+             * If the offset of e is to the right of the text we are to remove, do nothing
+             */
             if(e.getOffset() > event.getOffset() + event.getLength()) {
                 break;
-            } else if (e instanceof TextInsertEvent && e.getOffset() < event.getOffset()) {
+            }
+            /*
+             * If e is a TextInsertEvent and e's offset is to the left of the new event, add the length of
+             * the text inserted by e to the new events offset.
+             */
+            else if (e instanceof TextInsertEvent && e.getOffset() < event.getOffset()) {
                 event.setOffset(event.getOffset() + ((TextInsertEvent) e).getText().length());
-            } else if (e instanceof TextInsertEvent &&
+            }
+            /*
+             * If e is a TextInsertEvent to be inserted inside the text the new event is to remove, increase
+             * the lenght of the new event by the length of e's text. (remove e's text as well)
+             */
+            else if (e instanceof TextInsertEvent &&
                     e.getOffset() >= event.getOffset() &&
-                    e.getOffset() < event.getOffset()+event.getLength()) {
+                    e.getOffset() < event.getOffset() + event.getLength()) {
                 event.setLength(event.getLength() + ((TextInsertEvent) e).getText().length());
-            } else if (e instanceof TextRemoveEvent &&
+            }
+            /*
+             * If e is a TextRemoveEvent and e is to remove some text to the left of what the new event is removing,
+             * decrease the offset of the new event by the length of what e is removing.
+             */
+            else if (e instanceof TextRemoveEvent &&
                     e.getOffset() + ((TextRemoveEvent)e).getLength() < event.getOffset()) {
                 event.setOffset(event.getOffset() - ((TextRemoveEvent) e).getLength());
-            } else if (e instanceof TextRemoveEvent &&
+            }
+            /*
+             * If e is a TextRemoveEvent and e is removing the same or more text than the new event set the length
+             * of the new remove event to 0.
+             */
+            else if (e instanceof TextRemoveEvent &&
                     e.getOffset() <= event.getOffset() &&
                     e.getOffset() + ((TextRemoveEvent) e).getLength() >= event.getOffset() + event.getLength()) {
                 event.setLength(0);
-            } else if (e instanceof TextRemoveEvent &&
+            }
+            /*
+             * If e is a TextRemoveEvent and e overlapping some of the new event from the right, decrease the length
+             * of the new event by the length of the overlap.
+             */
+            else if (e instanceof TextRemoveEvent &&
                     e.getOffset() > event.getOffset() &&
                     e.getOffset() + ((TextRemoveEvent) e).getLength() > event.getOffset() + event.getLength()) {
                 event.setLength(event.getLength() - (event.getOffset() + event.getLength() - e.getOffset()));
-            } else if (e instanceof TextRemoveEvent &&
+            }
+            /*
+             * If e is a TextRemoveEvent and e is removing some of the same text as the new event, decrease the length
+             * of the new event by the length of e.
+             */
+            else if (e instanceof TextRemoveEvent &&
                     e.getOffset() > event.getOffset() &&
                     e.getOffset() + ((TextRemoveEvent) e).getLength() <= event.getOffset() + event.getLength()) {
                 event.setLength(event.getLength() - ((TextRemoveEvent) e).getLength());
-            } else if (e instanceof TextRemoveEvent &&
+            }
+            /*
+             * If e is a TextRemoveEvent, and e is overlapping the new event from the left, decrease the length of the
+             * new event by the length of the overlap, and increase the offset of the new event such that the new event
+             * begins where e ends.
+             */
+            else if (e instanceof TextRemoveEvent &&
                     e.getOffset() <= event.getOffset() &&
                     e.getOffset() + ((TextRemoveEvent) e).getLength() < event.getOffset() + event.getLength()) {
                 event.setLength(event.getOffset() + event.getLength() - (e.getOffset() + ((TextRemoveEvent) e).getLength()));
@@ -108,14 +158,37 @@ public class DocumentEventCapturer extends DocumentFilter {
     }
 
     private void updateOffsetOfInsertEvent(TextInsertEvent event) {
+        /*
+         * For every unexecuted local insert event the new event's offset has to be updated
+         * according to the following cases.
+         * e = unexecuted event.
+         */
         for(TextEvent e: unExecutedTextEvents) {
+            /*
+             * If e's offset is larger than the offset of the new event, do nothing.
+             */
             if(e.getOffset() > event.getOffset()) {
                 break;
-            } else if (e instanceof TextInsertEvent) {
+            }
+            /*
+             * If e is a TextInsertEvent with the same or smaller offset than the new event, increase the offset of
+             * the new event by the length of e's text.
+             */
+            else if (e instanceof TextInsertEvent) {
                 event.setOffset(event.getOffset() + ((TextInsertEvent) e).getText().length());
-            } else if (e instanceof TextRemoveEvent && event.getOffset() >= e.getOffset()+((TextRemoveEvent) e).getLength()) {
+            }
+            /*
+             * If e is a TextRemoveEvent and the new event is to the right of the end of e, decrease the offset of
+             * the new event by the length of the text removed by e.
+             */
+            else if (e instanceof TextRemoveEvent && event.getOffset() >= e.getOffset()+((TextRemoveEvent) e).getLength()) {
                 event.setOffset(event.getOffset() - ((TextRemoveEvent) e).getLength());
-            } else if (e instanceof TextRemoveEvent && event.getOffset() < e.getOffset()+((TextRemoveEvent) e).getLength()) {
+            }
+            /*
+             * If e is a TextRemoveEvent and the new event is writing inside some text to be removed by e, decrease the
+             * offset of the new event by the difference of the offsets of e and the new event.
+             */
+            else if (e instanceof TextRemoveEvent && event.getOffset() < e.getOffset()+((TextRemoveEvent) e).getLength()) {
                 event.setOffset(event.getOffset() - (event.getOffset() - e.getOffset()));
             }
         }
